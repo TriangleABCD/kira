28dee678067ea069dbf486b2d8a992c40bba048e
wangyaoyong <wangyaoyong@njust.edu.cn>
2025-05-26 23:14:00 +0800

    !project done

--- src/kira.py
+++ src/kira.py
@@ -1,8 +1,10 @@
 import kira_args, kira_config, kira_model
 
+kira_config_path = '/home/wyy/code/kira/config/kira.config.json'
+
 
 if __name__ == "__main__":
-    config = kira_config.Config('/home/wyy/code/kira/config/kira.config.json')
+    config = kira_config.Config(kira_config_path)
     args = kira_args.get_args(config)
 
     model = kira_model.Model(
@@ -14,16 +16,14 @@ if __name__ == "__main__":
        config.models[config.model_choice]['key_key_path'],
        config.models[config.model_choice]['encrypt'],
     )
-    
-    message = model.message
-    message.append({"role": "user", "content": "解释什么是伙伴系统"})
 
-    response = model.client.chat.completions.create(
-        model=config.models[config.model_choice]['v3'],
-        messages=message,
-        stream=True
-    )
-
-    for chunk in response:
-        if chunk.choices[0].delta.content is not None:
-            print(chunk.choices[0].delta.content, end="", flush=True)
+    if args.chat:
+        if args.model == config.models[config.model_choice]['v3']:
+            model.chat(args, config)
+        else:
+            model.reasonal_chat(args, config)
+    else:
+        if args.model == config.models[config.model_choice]['v3']:
+            model.chat_once(args, config)
+        else:
+            model.reasonal_chat_once(args, config)
--- src/kira_model.py
+++ src/kira_model.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
-from kira_utils import get_api_key, get_encrypted_api_key, multi_line_input
+import sys
+import subprocess
+from kira_utils import get_api_key, get_encrypted_api_key, multi_line_input, stream_output, stream_output_reason
 
 class Model:
     def __init__(self, vendor_name='', url='', v3='', r1='', key_path='', key_key_path='', encrypt=False):
@@ -20,6 +22,8 @@ class Model:
             "content": "你是领域专家，从专业角度回答用户问题，优先考虑专业性"
         }]
 
+        self.history = []
+
     def init_client(self):
         api_key = None
         if self.api_key_encrypt:
@@ -33,9 +37,162 @@ class Model:
         )
 
 
-    def chat_once(self):
-        pass
+    def chat_once(self, args, config):
+        user_input = sys.stdin.read()
+        if args.add:
+            print(user_input)
+        else:
+            print('[' + self.vendor_name + '-' + config.models[config.model_choice]['v3'] + ' 🤖 ]:')
+        pre_input = " ".join(args.non_option_args)
+        user_input = pre_input + "\n" + user_input
+
+        self.history.append(user_input)
+
+        message = self.message
+        message.append({"role": "user", "content": user_input})
+
+        response = self.client.chat.completions.create(
+            model=config.models[config.model_choice]['v3'],
+            messages=message,
+            stream=args.stream
+        )
+
+        print('')
+        
+        if args.stream:
+            content = stream_output(response)
+            self.history.append(content)
+        else:
+            print(response.choices[0].message.content)
+            self.history.append(response.choices[0].message.content)
 
 
-    def chat(self):
-        pass
+    def chat(self, args, config):
+        pre_input = " ".join(args.non_option_args)
+        message = self.message
+        while True:
+            print('🥰:')
+            user_input = multi_line_input()
+            if user_input == 'exit' or user_input == 'quit' or user_input == 'q':
+                break
+
+            if user_input.startswith('!'):
+                command = user_input[1:]
+                pre_input = ''
+                try:
+                    print('命令执行成功')
+                    result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
+                    pre_input = result.stdout + '\n' + result.stderr
+                    print(pre_input)
+                    message.append({"refusal":None, "annotations": None, "audio": None, "function_call": None, "tool_calls": None, "role": "assistant", "content": pre_input})
+                except subprocess.CalledProcessError as e:
+                    print('命令执行失败:', e)
+                continue
+
+            user_input = pre_input + '\n' + user_input
+            self.history.append(user_input)
+
+            message = self.message
+            message.append({"role": "user", "content": user_input})
+            message.append({"role": "user", "content": user_input})
+
+            response = self.client.chat.completions.create(
+                model=config.models[config.model_choice]['v3'],
+                messages=message,
+                stream=args.stream
+            )
+
+            print('')
+            cur_content = ""
+
+            if args.stream:
+                print('[' + self.vendor_name + '-' + config.models[config.model_choice]['v3'] + ' 🤖 ]:')
+                cur_content = stream_output(response)
+                self.history.append(cur_content)
+            else:
+                cur_content = response.choices[0].message.content
+                print(cur_content)
+                self.history.append(cur_content)
+            message.append({"refusal":None, "annotations": None, "audio": None, "function_call": None, "tool_calls": None, "role": "assistant", "content": cur_content})
+
+
+    def reasonal_chat_once(self, args, config):
+        user_input = sys.stdin.read()
+        if args.add:
+            print(user_input)
+            print('')
+        pre_input = " ".join(args.non_option_args)
+        user_input = pre_input + "\n" + user_input
+
+        self.history.append(user_input)
+
+        message = self.message
+        message.append({"role": "user", "content": user_input})
+
+        response = self.client.chat.completions.create(
+            model=config.models[config.model_choice]['r1'],
+            messages=message,
+            stream=args.stream
+        )
+        
+        if args.stream:
+            content = stream_output_reason(response, self.vendor_name + '-' + config.models[config.model_choice]['r1'])
+            self.history.append(content)
+        else:
+            print('[' + self.vendor_name + '-' + config.models[config.model_choice]['r1'] + ' 🤖🧐 ]:')
+            print(response.choices[0].message.reasoning_content)
+            print('')
+            print('[' + self.vendor_name + '-' + config.models[config.model_choice]['r1'] + ' 🤖 ]:')
+            print(response.choices[0].message.content)
+            self.history.append(response.choices[0].message.content)
+
+
+    def reasonal_chat(self, args, config):
+        pre_input = " ".join(args.non_option_args)
+        message = self.message
+        while True:
+            print('🥰:')
+            user_input = multi_line_input()
+            if user_input == 'exit' or user_input == 'quit' or user_input == 'q':
+                break
+
+            if user_input.startswith('!'):
+                command = user_input[1:]
+                pre_input = ''
+                try:
+                    print('命令执行成功')
+                    result = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
+                    pre_input = result.stdout + '\n' + result.stderr
+                    print(pre_input)
+                    message.append({"refusal":None, "annotations": None, "audio": None, "function_call": None, "tool_calls": None, "role": "assistant", "content": pre_input})
+                except subprocess.CalledProcessError as e:
+                    print('命令执行失败:', e)
+                continue
+
+            user_input = pre_input + '\n' + user_input
+            self.history.append(user_input)
+
+            message.append({"role": "user", "content": user_input})
+
+            response = self.client.chat.completions.create(
+                model=config.models[config.model_choice]['r1'],
+                messages=message,
+                stream=args.stream
+            )
+
+            print('')
+            cur_content = ""
+
+            if args.stream:
+                cur_content = stream_output_reason(response, self.vendor_name + '-' + config.models[config.model_choice]['r1'])
+                self.history.append(cur_content)
+            else:
+                print('[' + self.vendor_name + '-' + config.models[config.model_choice]['r1'] + ' 🤖🧐 ]:')
+                print(response.choices[0].message.reasoning_content)
+                print('')
+                print('[' + self.vendor_name + '-' + config.models[config.model_choice]['r1'] + ' 🤖 ]:')
+                cur_content = response.choices[0].message.content
+                print(cur_content)
+                self.history.append(cur_content)
+
+            message.append({"refusal":None, "annotations": None, "audio": None, "function_call": None, "tool_calls": None, "role": "assistant", "content": cur_content})
